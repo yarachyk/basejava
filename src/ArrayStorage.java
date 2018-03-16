8cf78c1d2e6ae36dfe44af7ef02403f20759166f
yach1117 <yaryna.chykalo@netcracker.com>
2018-03-16 11:35:13 +0200

    all meth

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -18,30 +18,47 @@ public class ArrayStorage {
     }
 
     Resume get(String uuid) {
+        Resume res = null;
+        for (int i=0; i<size; i++){
+            if (storage[i].uuid.equals(uuid)){
+                res = storage[i];
 
-        return null;
+            }
+        }
+        return res;
     }
 
     void delete(String uuid) {
+        for (int i=0; i<size; i++){
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i]=null;
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] newst = new Resume[size];
+        Resume[] newst = new Resume[size()];
+        int j = 0;
         for (int i=0; i<size; i++){
             if (storage[i]!=null){
-                newst[i]=storage[i];
+                newst[j]=storage[i];
+                j++;
             }
-            else break;
         }
         return newst;
     }
 
     int size() {
-        int size = getAll().length;
-
-       return size;
+        int size = 0;
+        for(int i=0; i<storage.length; i++){
+            if (storage[i]!=null){
+                size++;
+            }
+        }
+        return size;
     }
 }
